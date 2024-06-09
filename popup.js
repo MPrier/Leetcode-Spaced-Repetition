c46b2f43db26c654281c9c1965ae0a64f1727ba5
Matthew Prier <59553462+MPrier@users.noreply.github.com>
2024-06-09 16:49:42 -0400

    Make problem Id link to that leetcode problem and add temp logo

--- popup.js
+++ popup.js
@@ -10,7 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(element.problemId);
             if (element.problemId && element.difficulty) {
                 const li = document.createElement('li');
-                li.textContent = `Problem ID: ${element.problemId}, Difficulty: ${element.difficulty}`;
+                // Create a clickable link for the problem ID
+                const a = document.createElement('a');
+                a.href = `https://leetcode.com/problems/${element.problemId}`;
+                a.textContent = `Problem ID: ${element.problemId}`;
+                a.target = "_blank"; // Open link in a new tab
+
+                li.appendChild(a);
+                li.appendChild(document.createTextNode(`, Difficulty: ${element.difficulty}`));
                 objectList.appendChild(li);
             }
         });
