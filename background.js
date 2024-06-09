3ce9d53ec670a7ee6cb300bb52bf83c22b72739b
Matthew Prier <59553462+MPrier@users.noreply.github.com>
2024-06-09 15:07:05 -0400

    Store Problem name and difficulty chosen

--- background.js
+++ background.js
@@ -6,6 +6,7 @@ chrome.webRequest.onCompleted.addListener(
             const problemId = urlParts[problemIdIndex];
             
             console.log(`Problem submitted: ${problemId}`);
+            chrome.storage.local.set({ lastProblemSubmitted: problemId});
             
             chrome.windows.create({
                 url: chrome.runtime.getURL("difficulty.html"),
@@ -25,6 +26,20 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action == 'storeResponse') {
         console.log(request.option);
         
+        // Get and store the problem id and difficulty recorded for the last problem submitted
+        // FUTURE: 
+        // Add a check for the problem id in the completedProblemsList, 
+        // Store all difficulty ratings and date taken for each problem attempt 
+        chrome.storage.local.get({lastProblemSubmitted: null}, function (data) {
+            chrome.storage.local.get({completedProblemList: []}, function (result) {
+                var completedProblemList = result.completedProblemList;
+                var problemId = data.lastProblemSubmitted;
+                completedProblemList.push({problemId: problemId, difficulty: request.option});
+                console.log(completedProblemList);
+                chrome.storage.local.set({completedProblemList: completedProblemList});
+            })
+        });
+        // Close the difficulty popup when the answer if received
         chrome.storage.local.get('popupWindowId', (data) => {
             if (data.popupWindowId) {
               chrome.windows.remove(data.popupWindowId);
